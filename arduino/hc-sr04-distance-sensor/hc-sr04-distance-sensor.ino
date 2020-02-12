79210393cc6cbcb6e7ba289541566b5d569efc22
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2020-02-12 06:21:06 +0000

    added temperature reading

--- arduino/hc-sr04-distance-sensor/hc-sr04-distance-sensor.ino
+++ arduino/hc-sr04-distance-sensor/hc-sr04-distance-sensor.ino
@@ -2,6 +2,20 @@
     Arduino sketch for measuring distance using a HC-SR04 ultrasonic distance sensor. 
 */
 
+/** Tempeature Sensor settings **/
+// Include libraries needed for DS18B20 temperature sensor
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 0 on the Wemos D1 mini
+#define ONE_WIRE_BUS 0  //D3
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+float celcius=0;
+
+
 // define pin numbers
 const int trigPin = 4;  //D2
 const int echoPin = 5;  //D1
@@ -17,6 +31,13 @@ void setup() {
 }
 
 void loop() {
+    // call sensors.requestTemperatures() to issue a global temperature 
+    // request to all devices on the bus
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    
+    // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+    celcius=sensors.getTempCByIndex(0);
+
     // Clears the trigPin
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
@@ -32,9 +53,13 @@ void loop() {
     // Calculating the distance using speed of sound estimate
     distance = duration*0.034/2;
 
-    // Prints the distance on the Serial Monitor
+    // Prints the temperature and distance on the Serial Monitor
+    Serial.print("Temperature: ");
+    Serial.print(celcius);
+    Serial.print(" C   ");
     Serial.print("Distance: ");
-    Serial.println(distance);
+    Serial.print(distance);
+    Serial.println(" cm");
     delay(2000);
 }
 
